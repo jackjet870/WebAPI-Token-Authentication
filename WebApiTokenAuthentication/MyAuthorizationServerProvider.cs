f3bfe328948b9bba1bbf2175430ba7bef7d1fea9
Michael <casovita@gmail.com>
2016-10-26 15:11:31 +0300

    Web API whit Authentication is done

--- WebApiTokenAuthentication/MyAuthorizationServerProvider.cs
+++ WebApiTokenAuthentication/MyAuthorizationServerProvider.cs
@@ -2,10 +2,46 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using Microsoft.Owin.Security.OAuth;
+using System.Threading.Tasks;
+using System.Security.Claims;
 
 namespace WebApiTokenAuthentication
 {
-    public class MyAuthorizationServerProvider
+    public class MyAuthorizationServerProvider : OAuthAuthorizationServerProvider
     {
+        public override async Task ValidateClientAuthentication(OAuthValidateClientAuthenticationContext context)
+        {
+            context.Validated();  //
+        }
+
+        public override async Task GrantResourceOwnerCredentials(OAuthGrantResourceOwnerCredentialsContext context)
+        {
+            var identity = new ClaimsIdentity(context.Options.AuthenticationType);
+
+            if (context.UserName == "admin" && context.Password == "admin")
+            {
+                identity.AddClaim(new Claim(ClaimTypes.Role, "admin"));
+                identity.AddClaim(new Claim("username", "admin"));
+                identity.AddClaim(new Claim(ClaimTypes.Name, "Michael Roman"));
+                context.Validated(identity);
+
+            }
+            else if (context.UserName == "user" && context.Password == "user")
+            {
+                identity.AddClaim(new Claim(ClaimTypes.Role, "user"));
+                identity.AddClaim(new Claim("username", "user"));
+                identity.AddClaim(new Claim(ClaimTypes.Name, "John Smith"));
+                context.Validated(identity);
+            }
+            else
+            {
+                context.SetError("invalid_grant", "Provided username and password is incorrect");
+                return;
+            }
+            
+        }
+
+
     }
 }
